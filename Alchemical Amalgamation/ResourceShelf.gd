10dfb41db7d652125b1885aa9514c26ab9fc896e
Mark Chimes <mark-chimes@users.noreply.github.com>
2021-06-13 01:43:10 +0200

    New backgrounds and multiple ingredients on shelf.

--- Alchemical Amalgamation/ResourceShelf.gd
+++ Alchemical Amalgamation/ResourceShelf.gd
@@ -8,7 +8,8 @@ export (ResourceTypeFile.ResourceType) var resource_type = ResourceType.WATER
 export (String) var display_name = "Water"
 
 func _ready(): 
-	$RichTextLabel.text = display_name
+	$Label.text = display_name
+	$Label.visible = false
 	$Sprite.texture = load(ResourceTypeFile.sprite_path_for_resource_type(resource_type))
 
 signal drag_resource(resource_type)
@@ -19,9 +20,9 @@ func _on_ResourceArea_input_event(_viewport, event, shape_idx):
 
 func _on_ResourceArea_area_entered(area):
 	if area.name == "HoverHackArea":
-		$RichTextLabel.visible = true
+		$Label.visible = true
 
 func _on_ResourceArea_area_exited(area):
 	if area.name == "HoverHackArea":
-		$RichTextLabel.visible = false
+		$Label.visible = false
 
--- Alchemical Amalgamation/Workroom.gd
+++ Alchemical Amalgamation/Workroom.gd
@@ -11,12 +11,12 @@ func _ready():
 func _on_WasteBasket_destroy_resource():
 	emit_signal("destroy_resource")
 
-func _on_ShelfForIngredients_drag_resource(resource_type):
-	emit_signal("drag_resource_from_shelf", resource_type)
-
 func _on_Combinator_click_on_combinator_slot(slot_num):
 	print("WORKROOM COMBINATOR SLOT " + str(slot_num) + " CLICKED")
 	emit_signal("click_on_combinator_slot", slot_num)
 
 func set_combinator_slot_to_item(slot_num, resource_type): 
 	$Combinator.set_slot_to_item(slot_num, resource_type)
+
+func _on_ResourceForShelf_drag_resource(resource_type):
+	emit_signal("drag_resource_from_shelf", resource_type)
