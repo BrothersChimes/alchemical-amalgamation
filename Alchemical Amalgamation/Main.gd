d21dc62eb39e55ea531c39582458f76b8680d25d
Mark Chimes <mark-chimes@users.noreply.github.com>
2021-06-13 17:37:25 +0200

    New cauldron sprites.

--- Alchemical Amalgamation/Main.gd
+++ Alchemical Amalgamation/Main.gd
@@ -8,6 +8,7 @@ var book
 func _ready():
 	game_controller = $GameController
 	book = $Book
+	book.visible = true
 	remove_child(book)
 
 func _on_GameController_open_book():
@@ -15,24 +16,17 @@ func _on_GameController_open_book():
 
 func _process(delta):
 	if Input.is_action_just_pressed("book"):
-		print("IS BOOK OPEN? " + str(is_book_open))
 		if is_book_open:
 			close_book()
 		else: 
 			open_book()
 			
 func open_book():
-	print("OPENING BOOK")
-	print("BOOK: " + str(book))
-	print("GAME CONTROLLER: " + str(game_controller))
 	is_book_open = true
 	add_child(book)
 	remove_child(game_controller)
 	
 func close_book():
-	print("CLOSING BOOK")
-	print("BOOK: " + str(book))
-	print("GAME CONTROLLER: " + str(game_controller))
 	is_book_open = false
 	add_child(game_controller)
 	remove_child(book)
