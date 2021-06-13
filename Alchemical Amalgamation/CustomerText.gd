d2c0dc1d6eb3f2eb27df9f4d370f140c1997d42f
Mark Chimes <mark-chimes@users.noreply.github.com>
2021-06-13 19:41:18 +0200

    Customers show desired items.

--- Alchemical Amalgamation/CustomerText.gd
+++ Alchemical Amalgamation/CustomerText.gd
@@ -5,13 +5,13 @@ signal sell_potion_to(customer_number)
 func _on_Customer_click_on_customer(number):
 	emit_signal("sell_potion_to", number)
 
-func create_customer_with_message(customer_number, message): 
+func create_customer_with_message_and_item(customer_number, message, item_type): 
 	match customer_number:
-		0: $Customer0.set_customer_message(message)
-		1: $Customer1.set_customer_message(message)
-		2: $Customer2.set_customer_message(message)
-		3: $Customer3.set_customer_message(message)
-		4: $Customer4.set_customer_message(message)
+		0: $Customer0.set_customer_message_and_item(message,item_type)
+		1: $Customer1.set_customer_message_and_item(message,item_type)
+		2: $Customer2.set_customer_message_and_item(message,item_type)
+		3: $Customer3.set_customer_message_and_item(message,item_type)
+		4: $Customer4.set_customer_message_and_item(message,item_type)
 								
 func remove_customer(customer_number): 
 	match customer_number:
