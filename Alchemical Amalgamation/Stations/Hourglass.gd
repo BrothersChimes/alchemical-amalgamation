30508b8eb41d80a2731f8b5b9f78c08444d538db
Mark Chimes <mark-chimes@users.noreply.github.com>
2021-06-28 20:09:10 +0200

    Rotating hourglasses.

--- Alchemical Amalgamation/Stations/Hourglass.gd
+++ Alchemical Amalgamation/Stations/Hourglass.gd
@@ -2,8 +2,17 @@ extends Node2D
 
 func _on_Area2D_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.is_pressed() and event.button_index == BUTTON_LEFT:
-		$AnimatedSprite.play("sand")
+		if $AnimatedSprite.get_animation() == "done": 
+			$RotateAnimate.visible = true
+			$RotateAnimate.play("default")
+			$AnimatedSprite.visible = false
 
 func _on_AnimatedSprite_animation_finished():
 	if $AnimatedSprite.get_animation() == "sand": 
 		$AnimatedSprite.play("done")
+
+func _on_RotateAnimate_animation_finished():
+	$RotateAnimate.visible = false
+	$RotateAnimate.stop()
+	$AnimatedSprite.visible = true
+	$AnimatedSprite.play("sand")
